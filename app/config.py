0e7126a10c6c19f5691457188522e419bf69ed12
user <morevstas02@mail.ru>
2025-05-06 12:26:43 +0300

    Таблицы+ алембик

---
+++ app/config.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    BOT_TOKEN: str
+    # ADMINS_IDS: str
+    # DATABASE_SQLITE = 'sqlite+aiosqlite:///data/db.sqlite3'
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+    )
+
+    def get_db_url(self):
+        return (f"mysql+aiomysql://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+
+
+settings = Settings()
+
+bot = Bot(token=settings.BOT_TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bd = Dispatcher(storage=MemoryStorage())
+database_url = settings.DB_URL
